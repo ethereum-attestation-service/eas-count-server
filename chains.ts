731d8f87b4062d9aa5c9b56fa751da4b85935289
Steve Dakh <slavik0329@gmail.com>
2024-09-24 15:47:49 -0400

    Comment out Sepolia testnet configurations

--- chains.ts
+++ chains.ts
@@ -1,10 +1,10 @@
 import { Chain } from "./types";
 
 export const EAS_CHAIN_CONFIGS: Chain[] = [
-  {
-    id: "11155111",
-    subdomain: "sepolia.",
-  },
+  // {
+  //   id: "11155111",
+  //   subdomain: "sepolia.",
+  // },
   {
     id: "42161",
     subdomain: "arbitrum.",
@@ -17,18 +17,18 @@ export const EAS_CHAIN_CONFIGS: Chain[] = [
     id: "1",
     subdomain: "",
   },
-  {
-    id: "11155420",
-    subdomain: "optimism-sepolia.",
-  },
+  // {
+  //   id: "11155420",
+  //   subdomain: "optimism-sepolia.",
+  // },
   {
     id: "10",
     subdomain: "optimism.",
   },
-  {
-    id: "84532",
-    subdomain: "base-sepolia.",
-  },
+  // {
+  //   id: "84532",
+  //   subdomain: "base-sepolia.",
+  // },
   {
     id: "8453",
     subdomain: "base.",
@@ -37,10 +37,10 @@ export const EAS_CHAIN_CONFIGS: Chain[] = [
     id: "59144",
     subdomain: "linea.",
   },
-  {
-    id: "80001",
-    subdomain: "polygon-mumbai.",
-  },
+  // {
+  //   id: "80001",
+  //   subdomain: "polygon-mumbai.",
+  // },
   {
     id: "137",
     subdomain: "polygon.",
@@ -49,10 +49,10 @@ export const EAS_CHAIN_CONFIGS: Chain[] = [
     id: "534352",
     subdomain: "scroll.",
   },
-  {
-    id: "534351",
-    subdomain: "scroll-sepolia.",
-  },
+  // {
+  //   id: "534351",
+  //   subdomain: "scroll-sepolia.",
+  // },
   {
     id: "42220",
     subdomain: "celo.",
